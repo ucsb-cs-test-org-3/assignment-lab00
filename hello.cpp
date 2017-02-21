d43a167419ad21cbff55df4808f66af5eea87fab
pconrad-machine-acct-1 <pconrad.cis+machine-acct-1@gmail.com>
2017-02-21 12:28:46 -0800

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Your printing code should go here
+    cout << "Hello, world!" << endl;
+    cout << "CS 24 Winter 2017." << endl;
+
+    return 0;
+}
